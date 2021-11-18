c5fe7e5d5d5cbb8307665719b9f8cc1e805fef65
Nevin Chow <nevinchow@gmail.com>
2021-11-18 11:11:10 -0800

    .

--- migrations/versions/20211116_114130_.py
+++ migrations/versions/20211116_114130_.py
@@ -35,15 +35,15 @@ def upgrade():
     sa.Column('image', sa.String(length=255), nullable=True),
     sa.PrimaryKeyConstraint('id')
     )
-    op.create_table('users',
-    sa.Column('id', sa.Integer(), nullable=False),
-    sa.Column('username', sa.String(length=40), nullable=False),
-    sa.Column('email', sa.String(length=255), nullable=False),
-    sa.Column('hashed_password', sa.String(length=255), nullable=False),
-    sa.PrimaryKeyConstraint('id'),
-    sa.UniqueConstraint('email'),
-    sa.UniqueConstraint('username')
-    )
+    # op.create_table('users',
+    # sa.Column('id', sa.Integer(), nullable=False),
+    # sa.Column('username', sa.String(length=40), nullable=False),
+    # sa.Column('email', sa.String(length=255), nullable=False),
+    # sa.Column('hashed_password', sa.String(length=255), nullable=False),
+    # sa.PrimaryKeyConstraint('id'),
+    # sa.UniqueConstraint('email'),
+    # sa.UniqueConstraint('username')
+    # )
     # ### end Alembic commands ###
 
 
