39aae88e7233a799f68096dcb96482d18ef90689
Nevin Chow <nevinchow@gmail.com>
2021-11-22 14:12:35 -0800

    cleanup

--- react-app/src/components/HomePage/index.js
+++ react-app/src/components/HomePage/index.js
@@ -1,8 +1,17 @@
+import { useDispatch } from "react-redux"
+import { useEffect } from "react"
 import './HomePage.css'
-import PostsContainer from "../HomePagePosts"
 
+import PostsContainer from "../HomePagePosts"
+import { getComments } from "../../store/comment"
 
 function HomePage() {
+    const dispatch = useDispatch()
+
+
+    useEffect(() => {
+        dispatch(getComments())
+    }, [dispatch])
 
 
     return (
